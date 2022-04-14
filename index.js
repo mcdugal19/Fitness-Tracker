165e1f66dd4efc25ce020bc800fd161a3a704b2d
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-04-14 17:29:23 -0500

    added express server info

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // create the express server here
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use((req, res, next) => {
+    const token = req.header('token')
+    console.log('TOKEN->', token)
+    if(!token){
+        res.status(404).send('You are un-authorized!')
+        return
+    }
+})
+
+app.use('/users', require('../api/users'))
+
+app.use((error, req, res, next) => {
+    res.send({ success: false, message: error.message })
+})
+
+app.get('*', function (req, res, next) {
+  res.status(404).send("Sorry, we could'nt find that route!")
+})
+
+app.listen(PORT, function () {
+  console.log(`CORS-enabled web server listening on port ${PORT}`)
+})
